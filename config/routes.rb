a57a69d1f56120998d7eb81250ad036fd6ca73e4
Eric Fossoul <weloveworks@gmail.com>
2019-11-04 16:13:53 +0100

    "update"

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'questions#ask'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
+
